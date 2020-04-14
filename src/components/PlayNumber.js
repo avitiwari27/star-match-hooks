0352896f0b9e7e74276c3c403c6ab5c445b011ef
Avi tiwari <avithegr807@gmail.com>
2020-04-14 09:43:41 +0545

    updated components and utils

---
+++ src/components/PlayNumber.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../App.css";
+import colors from "../utils/colors";
+
+const PlayNumber = (props) => (
+  <button
+    className="number"
+    style={{ backgroundColor: colors[props.status] }}
+    onClick={() => props.onClick(props.number, props.status)}
+  >
+    {props.number}
+  </button>
+);
+
+export default PlayNumber;
